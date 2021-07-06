e833ff46303761cb57ded56f76175b24acb9eee1
sendy452 <sendyivenyulian452@gmail.com>
2021-07-06 22:17:16 +0700

    Fix add and refresh button

--- app/src/main/java/com/google/e_finance/ui/pendapatan/PendapatanFragment.java
+++ app/src/main/java/com/google/e_finance/ui/pendapatan/PendapatanFragment.java
@@ -1,18 +1,18 @@
 package com.google.e_finance.ui.pendapatan;
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.google.e_finance.R;
 
@@ -21,7 +21,7 @@ public class PendapatanFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_pendapatan, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
+        final TextView textView = root.findViewById(R.id.jumlah);
         setHasOptionsMenu(true);
         return root;
     }
@@ -30,5 +30,20 @@ public class PendapatanFragment extends Fragment {
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         inflater.inflate(R.menu.add_button, menu);
         super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+
+        MenuItem item = menu.findItem(R.id.btn_add);
+        Drawable icon = getResources().getDrawable(R.drawable.add);
+        icon.setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_IN);
+        MenuItem item2 = menu.findItem(R.id.btn_refresh);
+        Drawable icon2 = getResources().getDrawable(R.drawable.refresh);
+        icon2.setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_IN);
+
+        item.setIcon(icon);
+        item2.setIcon(icon2);
     }
 }
--- app/src/main/java/com/google/e_finance/ui/pengeluaran/PengeluaranFragment.java
+++ app/src/main/java/com/google/e_finance/ui/pengeluaran/PengeluaranFragment.java
@@ -1,9 +1,12 @@
 package com.google.e_finance.ui.pengeluaran;
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -30,5 +33,20 @@ public class PengeluaranFragment extends Fragment {
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         inflater.inflate(R.menu.add_button, menu);
         super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+
+        MenuItem item = menu.findItem(R.id.btn_add);
+        Drawable icon = getResources().getDrawable(R.drawable.add);
+        icon.setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_IN);
+        MenuItem item2 = menu.findItem(R.id.btn_refresh);
+        Drawable icon2 = getResources().getDrawable(R.drawable.refresh);
+        icon2.setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_IN);
+
+        item.setIcon(icon);
+        item2.setIcon(icon2);
     }
 }
